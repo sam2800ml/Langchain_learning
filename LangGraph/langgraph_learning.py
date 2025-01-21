e9da228cd15920aae76ccba5b941a5b67b545317
Santiago <sam2800ml@gmail.com>
2025-01-20 22:13:38 -0500

    solve

--- LangGraph/langgraph_learning.py
+++ LangGraph/langgraph_learning.py
@@ -7,7 +7,7 @@ from langgraph.graph.message import add_messages
 from transformers import AutoTokenizer, AutoModelForCausalLM
 from langchain_community.tools.tavily_search import TavilySearchResults
 import os
-os.environ["TAVILY_API_KEY"] = "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"
+
 
 tool = TavilySearchResults(max_results=1)
 tools = [tool]
